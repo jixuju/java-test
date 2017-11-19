cd10ff4718e8e4d546ca797f9b9a0d160357dfc4
jiangxj <jiangxj@wenwen-tech.com>
2017-11-19 17:32:15 +0800

    增加分布式ID解决方案

--- test1-encrypt/src/test/java/com/superwind/EncryptApplicationTests.java
+++ test1-encrypt/src/test/java/com/superwind/EncryptApplicationTests.java
@@ -5,10 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.UUID;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EncryptApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
